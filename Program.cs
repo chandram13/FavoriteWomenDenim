a50e432ace044f6b026a4ea0a1d638a85477c037
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-02 18:04:19 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+// Marvish Chandra
+
+namespace womenJeans{
+    class typeStraight{
+        public string 314c1;
+        public string 414;
+        public string 501;
+        public string 501nineties;
+        public string 701;
+        public string 724;
+    
+    static void femStraight{
+        typeStraight.314c1 = "Salsa Dark Stone";
+        typeStraight.315c2 = "Salsa Roll";
+    }
+    }
+    class typeSkinny{}
+    class typeLoose{}
+    class Bootcut{}
+    class Slim{}
+    class Tapered{}
+}
