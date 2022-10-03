6271b97e09260331c4442c74fdef831dd326bbcc
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-02 20:17:31 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,21 +2,92 @@
 
 namespace womenJeans{
     class typeStraight{
-        public string 314c1;
-        public string 414;
-        public string 501;
-        public string 501nineties;
-        public string 701;
-        public string 724;
-    
-    static void femStraight{
-        typeStraight.314c1 = "Salsa Dark Stone";
-        typeStraight.315c2 = "Salsa Roll";
+        public string premium314c1;
+        public string premium314c2;
+        public string premium314c3;
+        public string premium314c4;
+        public string premium314c5;
+        public string premium314c6;
+        public string premium314c7;
+        public string premium314c8;
+        public string premium314c9;
+        public string premium314c10;
+        public string premium314c11;
+        public string premium414plusc1;
+        public string premium414plusc2;
+        public string premium501c1;
+        public string premium501c2;
+        public string premium501c3;
+        public string premium501c4;
+        public string premium501c5;
+        public string premium501c6;
+        public string premium501c7;
+        public string premium501c8;
+        public string premium501c9;
+        public string premium501c10;
+        public string premium501c11;
+        public string premiumninetiesc1;
+        public string premiumninetiesc2;
+        public string premiumninetiesc3;
+        public string premiumninetiesc4;
+        public string premiumninetiesc5;
+        public string premiumninetiesc6;
+        public string premium701c1;
+        public string premium701c2;
+        public string premium701c3;
+        public string premium724c1;
+        public string premium724c2;
+        public string premium724c3;
+        public string premium724c4;
+        public string premium724c5;
+        public string premium724c6;
+        public string premium724c7;
+        public string premium724c8;
+        public string premium724c9;
+        public string premium724c10;
+        public string premium724c11;
+        public string premium724c12;
+  
+   
     }
+        static void straight(string[] args){
+
+        typeStraight.premium314c1 = "Jive Sound";
+        typeStraight.premium314c2 = "Montgomery Baked";
+        typeStraight.premium314c3 = "Oxnard Haze";
+        typeStraight.premium314c4 = "Charleston Outlasted";
+        typeStraight.premium314c5 = "After Sunset";
+        typeStraight.premium314c6 = "Black Sprout";
+        typeStraight.premium314c7 = "Salsa Roll";
+        typeStraight.premium314c8 = "Worn in Grey";
+        typeStraight.premium314c9 = "Salsa Dark Stone";
+        typeStraight.premium314c10 = "Break a Leg";
+        typeStraight.premium314c12 = "Meadow Mist";
+        
+
+        typeStraight.premium414plusc1 = "Lapis Dark Horse";
+        typeStraight.premium414plusc2 = "Marine Dip";
+
+        typeStraight.premium501c1 = "Troy Horse";
+        typeStraight.premium501c2 = "Luxor Last";
+        typeStraight.premium501c3 = "Worn in Medium Indigo";
+        typeStraight.premium501c4 = "Athens Dark Wash";
+        typeStraight.premium501c5 = "Worn in Dark Indigo";
+        typeStraight.premium501c6 = "Dark Brown Garment Dye";
+        typeStraight.premium501c7 = "Deep Breath";
+        typeStraight.premium501c8 = "Destructed Light Indigo";
+        typeStraight.premium501c9 = "Worn in Medium Indigo";
+
+        
+        }
+
+
+
     }
-    class typeSkinny{}
-    class typeLoose{}
-    class Bootcut{}
     class Slim{}
+    class Straight{}
     class Tapered{}
-}
+    class Relaxed{}
+    class Loose{}
+
+
